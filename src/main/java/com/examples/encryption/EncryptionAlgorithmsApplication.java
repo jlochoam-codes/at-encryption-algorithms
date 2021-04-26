f8909e1695e96e4a4cf87601344d1b8e21f7cf0b
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2021-04-26 15:18:10 -0500

    Created basic Spring Boot container

---
+++ src/main/java/com/examples/encryption/EncryptionAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.examples.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examples/encryption/EncryptionAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.encryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
