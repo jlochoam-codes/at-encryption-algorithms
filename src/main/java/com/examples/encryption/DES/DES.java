2a24f0a8726bbfcf6779033f87eb2a7805bec646
Blanca Galindo <blanca.galindo@agilethought.com>
2021-04-29 12:30:44 -0500

    Commit 3 DES

--- src/main/java/com/examples/encryption/DES/DES.java
+++ src/main/java/com/examples/encryption/DES/DES.java
@@ -1,87 +1,66 @@
 package com.examples.encryption.DES;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 public class DES {
 
     public static final String FILES_PATH = "src/main/resources/static/";
+    private static final int KEY_SIZE = 56;
+    private static final String ALGORITHM = "DES/ECB/PKCS5Padding";
 
     public static void generateKey(String [] args) throws Exception{
 
-        String comand1 = "-c";
-        String comand2 = "-d";
+        KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+        keyGen.init(KEY_SIZE);
+        SecretKey key = keyGen.generateKey();
+        BufferedWriter keyFileWriter = Files.newBufferedWriter(
+                Paths.get(FILES_PATH + "keyFile.txt"));
+        keyFileWriter.write(Base64.getEncoder().encodeToString(key.getEncoded()));
+        keyFileWriter.close();
 
-        //COMAND 1 o COMAND 2
-        if ((comand1.equals(args[0]))||(comand2.equals(args[0]))){
-            //leer clave por teclado
-            try{
-                InputStreamReader read_key = new InputStreamReader(System.in);
-                BufferedReader buff_key = new BufferedReader(read_key);
-                System.out.print("Write a key: ");
-                String key = buff_key.readLine();
+        /* PASO 2: Crear cifrador */
+        Cipher encryptor= Cipher.getInstance(ALGORITHM);
 
-                //pasar clave a la clase SecretKey
-                try{
-                    SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
-                    DESKeySpec kspec = new DESKeySpec(key.getBytes());
-                    SecretKey ks = skf.generateSecret(kspec);
+        /* PASO 3a: Inicializar cifrador en modo CIFRADO */
+        encryptor.init(Cipher.ENCRYPT_MODE, key);
 
-                    //Inicializar el cifrado
-                    try{
-                        Cipher encryption = Cipher.getInstance("DES");
+        /* Leer fichero de 1k en 1k y pasar fragmentos leidos al cifrador */
+        byte[] bufferPlano = new byte[1000];
+        byte[] bufferCifrado;
 
-                        //Escojo modo cifrado o descifrado segun sea el caso
-
-                        if (comand1.equals(args[0])){
-                            encryption.init(Cipher.ENCRYPT_MODE, ks);}//MODO CIFRAR
-                        if (comand2.equals(args[0])){
-                            encryption.init(Cipher.DECRYPT_MODE, ks);}//MODO DESCIFRAR
-
-
-                        //Leer fichero
-
-                        InputStream file = new FileInputStream( args[1] );
-                        OutputStream fich_out = new FileOutputStream ( args[2] );
-
-                        byte[] buffer = new byte[1024];
-                        byte[] bloque_cifrado;
-                        String textCrypted = new String();
-                        int fin_file = -1;
-                        int read;//numero de bytes leidos
-
-                        read = file.read(buffer);
-
-                        while( read != fin_file ) {
-                            bloque_cifrado = encryption.update(buffer,0,read);
-                            textCrypted = textCrypted + new String(bloque_cifrado,"ISO-8859-1");
-                            read = file.read(buffer);
-                        }
-
-                        file.close();
-
-                        bloque_cifrado = encryption.doFinal();
-                        textCrypted = textCrypted + new String(bloque_cifrado,"ISO-8859-1");
-                        //ISO-8859-1 es ISO-Latin-1
-
-                        fich_out.write(textCrypted.getBytes("ISO-8859-1"));//escribir fichero
-
-                    }
-                    //Inicializacion de cifrado
-                    catch(javax.crypto.NoSuchPaddingException nspe) {} //Instanciacion DES
-                    catch(javax.crypto.IllegalBlockSizeException ibse) {}//metodo doFinal
-                    catch(javax.crypto.BadPaddingException bpe) {}//metodo doFinal
-                }
-                //pasar clave a la clase SecretKey
-                catch(java.security.InvalidKeyException ike) {}
-                catch(java.security.spec.InvalidKeySpecException ikse) {}
-                catch(java.security.NoSuchAlgorithmException nsae) {}
-            }
-            //leer del teclado la clave como String
-            catch(java.io.IOException ioex) {}
+        String textoCifradoTotal = new String();
+        FileInputStream in = new FileInputStream(args[0]);
+        int bytesLeidos = in.read(bufferPlano,0, 1000);
+        while(bytesLeidos != -1) {  // Mientras no se llegue al final del fichero
+            bufferCifrado = encryptor.update(bufferPlano, 0 , bytesLeidos);  // Pasa texto claro leido al cifrador
+            textoCifradoTotal = textoCifradoTotal + new String(bufferCifrado); // Acumular texto cifrado
+            bytesLeidos = in.read(bufferPlano,0, 1000);
         }
+        in.close();
+
+        bufferCifrado = encryptor.doFinal(); // Completar cifrado (puede devolver texto)
+        textoCifradoTotal = textoCifradoTotal + new String(bufferCifrado);
+
+
+        System.out.println("--------------- TEXTO CIFRADO ---------------");
+        System.out.println(textoCifradoTotal);   // Mostrar texto cifrado
+        System.out.println("---------------------------------------------");
+
+        System.out.println("--------------- TEXTO DESCIFRADO -------------");
+        /* PASO 3b: Poner cifrador en modo DESCIFRADO */
+        encryptor.init(Cipher.DECRYPT_MODE, key);
+        byte[] textoDescifrado = encryptor.update(textoCifradoTotal.getBytes()); // Pasar texto al descifrador
+        System.out.print( new String(textoDescifrado) );
+        textoDescifrado = encryptor.doFinal(); // Completar descifrado (puede devolver texto)
+        System.out.print( new String(textoDescifrado) );
+        System.out.println("----------------------------------------------");
     }
 }
